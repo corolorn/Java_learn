1f390db62ceded1d22577789ef2003379a28b297
Paul <corolorn@gmail.com>
2017-07-14 22:15:06 +0300

    Поиск min и max в массиве

---
+++ ArrayMax_Min.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import static java.lang.Math.*;
+public class ArrayMax {
+    public static void main(String[] args) throws Exception {
+      // Создаем ридер с консоли
+      
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int setArray = Integer.parseInt(br.readLine());
+        
+      // Обьявляем массив размером полученным с консоли и нужные переменные
+        int array[] =  new int[setArray];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+      // Цикл заполняет массив 
+        for (int i = 0;i<setarray;i++){
+        array[i]=Integer.parseInt(br.readLine());}
+        
+      // Цикл поиска в массиве максимального и минимального числа
+        for (int j = 0; j < array.length; j++){
+        max = max(max, array[j]);
+        min = min(min, array[j]);}
+        
+     // Вывод максимального и минимального 
+        System.out.println(max);
+        System.out.println(min)
+
+        }
+
+    }
